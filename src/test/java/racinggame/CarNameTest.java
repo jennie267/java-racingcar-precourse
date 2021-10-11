1a9c082f6e75ffea70278855281cd3a230ffa590
sooki <jennieee267@gmail.com>
2021-10-11 21:46:10 +0900

    refactor : CarName에 대한 테스트 분리

---
+++ src/test/java/racinggame/CarNameTest.java
@@ -0,0 +1,30 @@
+package racinggame;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarNameTest {
+	private Car car;
+
+	@BeforeEach
+	void setUp() {
+		car = new Car("jen");
+	}
+	
+	@DisplayName("자동차 이름 5자 이하만 가능")
+	@Test
+	void 이름_5자_이하() {
+		assertThat(car.getCarName().checkLenght()).isTrue();
+	}
+
+	@DisplayName("자동차 이름 5자 넘으면 오류")
+	@Test
+	void 이름_5자_초과() {
+		assertThrows(IllegalArgumentException.class, () -> new Car("jennie"));
+	}
+
+}
