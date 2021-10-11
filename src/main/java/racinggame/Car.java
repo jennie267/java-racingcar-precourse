a526240179f316d13dbe86c80d2092fbf5330ac8
sooki <jennieee267@gmail.com>
2021-10-11 18:38:27 +0900

    feat : 자동차는 random값에 따라 전진, 정지한다.

--- src/main/java/racinggame/Car.java
+++ src/main/java/racinggame/Car.java
@@ -2,13 +2,29 @@ package racinggame;
 
 public class Car {
 	private final String name;
+	private int position;
 
 	public Car(String name) {
 		this.name = name;
+		this.position = 0;
 	}
 
 	public String getName() {
 		return this.name;
+	}
+
+	private void go() {
+		this.position++;
+	}
+
+	public int getPosition() {
+		return this.position;
+	}
+
+	public void move(int randomNumber) {
+		if (randomNumber >= 4) {
+			go();
+		}
 	}
 
 }
