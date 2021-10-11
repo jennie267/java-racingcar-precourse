0c2e4dfe192e089c10671235d04573a5e9c751aa
sooki <jennieee267@gmail.com>
2021-10-11 19:06:42 +0900

    test : 자동차 이름은 5자 이하만 가능하다.

--- src/test/java/racinggame/CarTest.java
+++ src/test/java/racinggame/CarTest.java
@@ -1,8 +1,10 @@
 package racinggame;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
@@ -10,12 +12,12 @@ public class CarTest {
 
 	@BeforeEach
 	void setUp() {
-		car = new Car("jennie");
+		car = new Car("jen");
 	}
 
 	@Test
 	void 이름_부여() {
-		assertThat(car.getName()).isEqualTo("jennie");
+		assertThat(car.getCarName().isName("jen")).isTrue();
 	}
 
 	@Test
@@ -37,6 +39,18 @@ public class CarTest {
 		car.move(5);
 		car.move(7);
 		assertThat(car.getPosition()).isEqualTo(2);
+	}
+	
+	@DisplayName("자동차 이름 5자 이하만 가능")
+	@Test
+	void 이름_5자_이하() {
+		assertThat(car.getCarName().checkLenght()).isTrue();
+	}
+	
+	@DisplayName("자동차 이름 5자 넘으면 오류")
+	@Test
+	void 이름_5자_초과() {
+		assertThrows(IllegalArgumentException.class,() -> new Car("jennie"));
 	}
 
 }
