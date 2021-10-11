a20ea309211e89ef5e404c0d188c94b6222b0e4b
sooki <jennieee267@gmail.com>
2021-10-11 18:38:13 +0900

    test : 자동차는 random값에 따라 전진, 정지한다.

--- src/test/java/racinggame/CarTest.java
+++ src/test/java/racinggame/CarTest.java
@@ -2,14 +2,32 @@ package racinggame;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
+	private Car car;
+
+	@BeforeEach
+	void setUp() {
+		car = new Car("jennie");
+	}
 
 	@Test
 	void 자동차_이름_부여() {
-		Car car = new Car("jennie");
 		assertThat(car.getName()).isEqualTo("jennie");
+	}
+
+	@Test
+	void 자동차_전진() {
+		car.move(4);
+		assertThat(car.getPosition()).isEqualTo(1);
+	}
+	
+	@Test
+	void 자동차_정지() {
+		car.move(3);
+		assertThat(car.getPosition()).isEqualTo(0);
 	}
 
 }
