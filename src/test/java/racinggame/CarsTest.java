2d1ec6579135f2f80c705b993507b9c5f6cecf8b
sooki <jennieee267@gmail.com>
2021-10-11 20:41:53 +0900

    test : 랜덤값 생성 테스트 추가

--- src/test/java/racinggame/CarsTest.java
+++ src/test/java/racinggame/CarsTest.java
@@ -2,18 +2,29 @@ package racinggame;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarsTest {
+	private Cars cars;
+
+	@BeforeEach
+	void setUp() {
+		cars = new Cars("car1,car2,car3");
+	}
 
 	@DisplayName("자동차 이름은 쉼표 기준으로 여러대 입력받는다.")
 	@Test
 	void 여러대_입력() {
-		Cars cars = new Cars("car1,car2,car3");
 		cars.countCar();
 		assertThat(cars.countCar()).isEqualTo(3);
-
 	}
 
+	@DisplayName("0에서 9사이의 랜덤값 생성")
+	@Test
+	void 랜덤값_생성() {
+		int randomNumber = cars.makeRandomNumber();
+		assertThat(randomNumber > -1 && randomNumber <10).isTrue();
+	}
 }
