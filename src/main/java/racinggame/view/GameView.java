abf7d6da1a33eecc4827cdb2bd03d663930415cf
sooki <jennieee267@gmail.com>
2021-10-11 20:25:31 +0900

    feat : 사용자에게 게임 횟수를 입력받는다. - 잘못된 값 입력시 재입력을 받는다.

--- src/main/java/racinggame/view/GameView.java
+++ src/main/java/racinggame/view/GameView.java
@@ -9,8 +9,19 @@ public class GameView {
 		return Console.readLine();
 	}
 	
+	public int enterTryNumber() {
+		System.out.println("시도할 회수는 몇회인가요?");
+		try {
+			return Integer.parseInt(Console.readLine());
+		} catch (NumberFormatException e) {
+			System.out.println("[ERROR] 숫자만 입력 가능합니다.");
+			return enterTryNumber();
+		}
+	}
+
 	public void printErrorMessage(String error) {
 		System.out.println(error);
 	}
+	
 
 }
