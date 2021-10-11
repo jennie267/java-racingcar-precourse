cafc0919022fbd100d84766de1f3d9c76a0ca98f
sooki <jennieee267@gmail.com>
2021-10-11 19:46:21 +0900

    feat : 자동차 이름은 쉼표 기준으로 여러대 입력 받는다.

---
+++ src/main/java/racinggame/Cars.java
@@ -0,0 +1,26 @@
+package racinggame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+	private final List<Car> cars;
+
+	public Cars(String inputCars) {
+		this.cars = mapCar(inputCars);
+	}
+	
+	private static List<Car> mapCar(String inputCars) {
+		String[] splitCars = inputCars.split(",");
+		List<Car> cars = new ArrayList<Car>();
+		for (String car : splitCars) {
+			cars.add(new Car(car));
+		}
+		return cars;
+	}
+
+	public int countCar() {
+		return cars.size();
+	}
+
+}
